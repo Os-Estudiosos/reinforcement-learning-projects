21f152703f0d8455ed6c3530699857d639da5a93
jaopredo <joao.profissional.45@gmail.com>
2025-08-29 16:24:00 -0300

    Saving the modifications made until now. Started making the ambient class

--- recycle-robot/main.py
+++ recycle-robot/main.py
@@ -1,8 +1,10 @@
-from robot import *
+from src.ambient import Ambient
 
 
 def main():
-    ...
+    ambient = Ambient()
+
+    ambient.run()
 
 
 if __name__ == "__main__":
--- recycle-robot/src/ambient.py
+++ recycle-robot/src/ambient.py
@@ -1,6 +1,22 @@
 import gymnasium
+from src.robot import Robot
 
 
 class Ambient:
-    def __init__(self):
-        pass
+    def __init__(self, alpha, beta, learning_rate, epochs):
+        self.alpha = alpha
+        self.beta = beta
+        self.learning_rate = learning_rate
+
+        self.agent = Robot(learning_rate)
+
+        self.EPOCHS = epochs
+    
+    def process_reward(self, action):
+        ...
+    
+    def run(self):
+        for _ in range(self.EPOCHS):
+            action = self.agent.choose()
+
+            reward = self.process_reward(action)
